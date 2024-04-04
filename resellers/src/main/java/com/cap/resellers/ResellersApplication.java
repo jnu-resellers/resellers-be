1cab3de86cb7a14e359d014bb207290e64f8913a
hwangdaesun <hds650588@gmail.com>
2024-04-04 16:49:51 +0900

    feat/#1 : 프로젝트 초기 세팅

---
+++ resellers/src/main/java/com/cap/resellers/ResellersApplication.java
@@ -0,0 +1,13 @@
+package com.cap.resellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellersApplication.class, args);
+	}
+
+}
