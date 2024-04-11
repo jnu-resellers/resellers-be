989baeaa7f12f2c9b9a6350107c0522d2508b0c4
hwangdaesun <hds650588@gmail.com>
2024-04-12 02:30:37 +0900

    feat : 멘토링 질문 리스트 조회 API 구현

---
+++ resellers/src/main/java/com/cap/resellers/mentoring/repository/MentoringRepository.java
@@ -0,0 +1,7 @@
+package com.cap.resellers.mentoring.repository;
+
+import com.cap.resellers.mentoring.model.Mentoring;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
+}
