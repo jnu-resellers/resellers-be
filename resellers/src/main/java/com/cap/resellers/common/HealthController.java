f449542ff0eb47476671360c41c95500ad56f698
hwangdaesun <hds650588@gmail.com>
2024-05-09 00:30:03 +0900

    feat : health check

--- resellers/src/main/java/com/cap/resellers/common/HealthController.java
+++ resellers/src/main/java/com/cap/resellers/common/HealthController.java
@@ -1,12 +1,13 @@
 package com.cap.resellers.common;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HealthController {
+
+    @GetMapping("/health")
     public ApiResponse<ApiResponse.CustomBody<Void>> health() {
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
