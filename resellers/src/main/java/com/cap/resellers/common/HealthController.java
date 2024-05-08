45003c06bf3fe4556d194b813fd872ec3aa4c2ae
hwangdaesun <hds650588@gmail.com>
2024-05-08 18:33:54 +0900

    feat : 스키마 반영

--- resellers/src/main/java/com/cap/resellers/common/HealthController.java
+++ resellers/src/main/java/com/cap/resellers/common/HealthController.java
@@ -1,2 +1,13 @@
-package com.cap.resellers.common;public class HealthController {
+package com.cap.resellers.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+    public ApiResponse<ApiResponse.CustomBody<Void>> health() {
+        return ApiResponseGenerator.success(HttpStatus.OK);
+    }
 }
