599b1891c933ce80afdc4e16873991b4740b0fe4
hwangdaesun <hds650588@gmail.com>
2024-05-17 00:45:09 +0900

    feat : data.sql 수정

--- src/main/java/com/cap/resellers/material/model/ItemType.java
+++ src/main/java/com/cap/resellers/material/model/ItemType.java
@@ -1,12 +1,17 @@
 package com.cap.resellers.material.model;
 
 public enum ItemType {
-    REFRIGERATOR("냉장고"),
-    CHOPSTICKS("젓가락"),
-    BOWL("그릇"),
+    REFRIGERATOR("냉장고/냉동고"),
+    SHOW_CASE("쇼케이스"),
+    FURNITURE("가구"),
+    GASRANGE("가스레인지"),
+    PACKING_MACHINE("포장기계"),
+    WASHING_MACHINE("세척기"),
     ICE_MAKER("제빙기"),
-    SINK("싱크대"),
-    OVEN("오븐");
+    SINK("싱크대/작업대"),
+    AIR_CONDITION("에어컨"),
+    KITCHEN_GOODS("주방잡화"),
+    COFFE_MACHINE("커피머신");
 
     private String value;
 
