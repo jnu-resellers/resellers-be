2378f108418e3e8adf64cc65692c5b71f19ffbdb
hwangdaesun <hds650588@gmail.com>
2024-05-21 16:51:35 +0900

    feat : request 스키마 변경

--- src/main/java/com/cap/resellers/material/dto/request/CreateMaterialRequest.java
+++ src/main/java/com/cap/resellers/material/dto/request/CreateMaterialRequest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public record CreateMaterialRequest(
         @Schema(defaultValue = "LG 냉장고", description = "상품명") String productName,
-        @Schema(defaultValue = "냉장고") String itemType,
+        @Schema(defaultValue = "냉장고/냉동고") String itemType,
         @Schema(defaultValue = "파일명.jpeg") List<String> fileNames,
         @Schema(defaultValue = "100000", description = "상품 가격") Integer price,
         @Schema(defaultValue = "어쩌구 저쩌구", description = "설명") String description,
