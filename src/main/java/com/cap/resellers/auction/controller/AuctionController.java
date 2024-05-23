522c7625aad608e793aff1dfaa49df97fad99c34
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-23 17:09:01 +0900

    feat: 기자재 경매 생성

--- src/main/java/com/cap/resellers/auction/controller/AuctionController.java
+++ src/main/java/com/cap/resellers/auction/controller/AuctionController.java
@@ -6,19 +6,26 @@ import com.cap.resellers.common.ApiResponse;
 import com.cap.resellers.common.ApiResponseGenerator;
 import com.cap.resellers.material.service.CreateMaterialService;
 import com.cap.resellers.material.service.UploadImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
+@Tag(name = "기자재(경매)", description = "Auction API")
 public class AuctionController {
     private final CreateAuctionService createAuctionService;
     private final UploadImageService uploadImageService;
     private final CreateMaterialService createMaterialService;
 
-
+    @PostMapping("/auction")
+    @Operation(summary = "경매 생성", description = "경매를 생성합니다, 경매를 생성하는 사람은 항상 member_pk가 1입니다")
     public ApiResponse<ApiResponse.CustomBody<Void>> createAuction(@RequestBody CreateAuctionRequest request) {
         Long memberId = 1L;
         createAuctionService.execute(request, memberId);
