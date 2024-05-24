20c6ba36d4acfb3c8ea3007601b6eb45dd326858
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-24 14:33:28 +0900

    fix: cors 수정

--- src/main/java/com/cap/resellers/config/WebConfig.java
+++ src/main/java/com/cap/resellers/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ocalhost:5173")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .maxAge(3600);
     }
