f312d3d412e9b4c4db9ed0be8c8f2e869af4421f
hwangdaesun <hds650588@gmail.com>
2024-05-27 23:04:56 +0900

    feat : 날짜 타입 응답 포맷팅

--- src/main/java/com/cap/resellers/auction/dto/response/GetAuctionsDTO.java
+++ src/main/java/com/cap/resellers/auction/dto/response/GetAuctionsDTO.java
@@ -1,5 +1,6 @@
 package com.cap.resellers.auction.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -9,7 +10,9 @@ public record GetAuctionsDTO(
         String itemType,
         String productName,
         Integer bidCount,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime startAt,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime endAt,
         Integer price,
         Long auctionId
