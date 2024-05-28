b9e4ffb44d5e61376da82d840fa08b55f57004be
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-28 15:02:10 +0900

    fix: 기자재 상세조회 수정
    - productId, materalId 응답에 포함

--- src/main/java/com/cap/resellers/material/dto/GetMaterialProductDto.java
+++ src/main/java/com/cap/resellers/material/dto/GetMaterialProductDto.java
@@ -6,11 +6,11 @@ import lombok.Builder;
 import java.util.List;
 import java.util.Optional;
 @Builder
-public record GetMaterialProductDto(List<String> fileNames, Long id, String productName, Integer price, String description, String defect, boolean isSold) {
+public record GetMaterialProductDto(List<String> fileNames, Long productId, String productName, Integer price, String description, String defect, boolean isSold) {
     public static GetMaterialProductDto of(List<String> fileNames, Product product) {
         return GetMaterialProductDto.builder()
                 .fileNames(fileNames)
-                .id(product.getId())
+                .productId(product.getId())
                 .productName(product.getName())
                 .price(product.getPrice())
                 .description(product.getDescription())
--- src/main/java/com/cap/resellers/material/dto/response/GetMaterialResponse.java
+++ src/main/java/com/cap/resellers/material/dto/response/GetMaterialResponse.java
@@ -3,12 +3,13 @@ package com.cap.resellers.material.dto.response;
 import com.cap.resellers.material.dto.GetMaterialProductDto;
 import lombok.Builder;
 @Builder
-public record GetMaterialResponse(String writer, GetMaterialProductDto product, String contact) {
-    public static GetMaterialResponse of(String writer, GetMaterialProductDto product, String contact) {
+public record GetMaterialResponse(String writer, GetMaterialProductDto product, String contact, Long materialId) {
+    public static GetMaterialResponse of(String writer, GetMaterialProductDto product, String contact, Long materialId) {
         return GetMaterialResponse.builder()
                 .writer(writer)
                 .product(product)
                 .contact(contact)
+                .materialId(materialId)
                 .build();
     }
 }
