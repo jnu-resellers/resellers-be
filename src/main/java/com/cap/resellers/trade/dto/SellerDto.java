e5f9ed9b26cc15c698fdb81bad3a88471d4f3ba9
hwangdaesun <hds650588@gmail.com>
2024-06-08 20:39:44 +0900

    feat : name 추가

--- src/main/java/com/cap/resellers/trade/dto/SellerDto.java
+++ src/main/java/com/cap/resellers/trade/dto/SellerDto.java
@@ -7,12 +7,14 @@ import lombok.Builder;
 public record SellerDto(
         String contact,
         String accountNumber,
-        String bankName) {
+        String bankName,
+        String name) {
     public static SellerDto from(Material material) {
         return SellerDto.builder()
                 .contact(material.getContact())
                 .accountNumber(material.getMember().getAccountNumber())
                 .bankName(material.getMember().getBankName())
+                .name(material.getMember().getName())
                 .build();
     }
 }
