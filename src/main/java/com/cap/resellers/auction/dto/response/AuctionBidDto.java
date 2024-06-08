9a11eb2618278b6dd737a2f6708cad28c6df098c
hwangdaesun <hds650588@gmail.com>
2024-06-08 20:27:35 +0900

    feat : 경매 입찰 가격 목록 조회에서 createAt 응답에서 yyyy 제거

--- src/main/java/com/cap/resellers/auction/dto/response/AuctionBidDto.java
+++ src/main/java/com/cap/resellers/auction/dto/response/AuctionBidDto.java
@@ -1,10 +1,14 @@
 package com.cap.resellers.auction.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record AuctionBidDto(
         String nickname,
         Integer price,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime createdAt) {
     public static AuctionBidDto of(String nickname, Integer price, LocalDateTime createdAt) {
         return new AuctionBidDto(nickname, price, createdAt);
